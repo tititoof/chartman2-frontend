96a8f1c3d4140fe796d1c650aee7e85d4c3c1f34
Christophe Hartmann <chartmann.35@gmail.com>
2021-12-25 07:55:39 +0100

    ci(sonarqube): remove duplicate block

---
+++ utils/data.js
@@ -0,0 +1,50 @@
+export const defaultFeatures = [
+  {
+    avatarColor: 'lime',
+    title: 'Idée',
+    content: 'Imagine ta nouvelle fonctionnalité.',
+    textContentClassProp: 'mx-4',
+    icon: 'mdi-head-lightbulb-outline',
+    iconHeight: 72
+  },
+  {
+    title: 'Histoire',
+    content: 'Décrit ton idée comme une (ou des) histoires dans ta gestion de projet (OpenProject)',
+    textContentClassProp: 'mx-4',
+    icon: 'mdi-book-open-variant',
+    iconHeight: 72,
+    avatarColor: 'primary'
+  },
+  {
+    title: 'Développement',
+    content: 'Développe ton (ou tes) histoires ainsi que tes tests',
+    textContentClassProp: 'mx-4',
+    icon: 'mdi-dev-to',
+    iconHeight: 72,
+    avatarColor: 'primary'
+  },
+  {
+    title: 'Validation',
+    content: 'Valide ton développement avec le pipeline de Jenkins, <br/>lançant tes tests automatiquement (test framework)<br/> et validant ta qualité de code (SonarQube)',
+    textContentClassProp: 'mx-4',
+    icon: 'mdi-test-tube',
+    iconHeight: 72,
+    avatarColor: 'orange'
+  },
+  {
+    title: 'Déploiement',
+    content: 'Déploie ton code en test ou en production avec le pipeline de Jenkins',
+    textContentClassProp: 'mx-4',
+    icon: 'mdi-rocket-launch-outline',
+    iconHeight: 72,
+    avatarColor: 'teal'
+  },
+  {
+    title: 'Bénéfices',
+    content: 'Profite de la nouvelle fonctionnalité dès qu\'elle disponible',
+    textContentClassProp: 'mx-4',
+    icon: 'mdi-emoticon-happy-outline',
+    iconHeight: 72,
+    avatarColor: 'indigo'
+  }
+]
