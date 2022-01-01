cb8620307cb628d9589da926a58eb8decb1c08b9
Christophe Hartmann <chartmann.35@gmail.com>
2022-01-01 13:25:28 +0100

    fix(lint): ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,17 +8,17 @@ module.exports = {
       args: 'start'
     }
   ],
-   
+
   // Deployment Configuration
-  deploy : {
-    production : {
-      "key"  : "/home/toofytroll/Projets/dockers/deploy-key-rsa",
-      "user" : "deploy",
-      "host" : ["51.75.194.189"],
-      "ref"  : "origin/main",
-      "repo" : "https://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@github.com/tititoof/chartman2-frontend.git",
-      "path" : "/var/www/websites/chartman2.fr/frontend",
-      "post-deploy" : "yarn install && yarn build && pm2 startOrRestart ecosystem.config.js --env production"
+  deploy: {
+    production: {
+      key: '/home/toofytroll/Projets/dockers/deploy-key-rsa',
+      user: 'deploy',
+      host: ['51.75.194.189'],
+      ref: 'origin/main',
+      repo: 'https://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@github.com/tititoof/chartman2-frontend.git',
+      path: '/var/www/websites/chartman2.fr/frontend',
+      'post-deploy': 'yarn install && yarn build && pm2 startOrRestart ecosystem.config.js --env production'
     }
   }
 }
