f325d87932293c9b76b731114a1b593b852ab1c8
Christophe Hartmann <chartmann.35@gmail.com>
2022-01-08 08:59:08 +0100

    fix(tests): add more

--- test/components/Blog/Presentation.spec.js
+++ test/components/Blog/Presentation.spec.js
@@ -61,9 +61,22 @@ describe('Presentation', () => {
 
     expect(routerMock.push).toHaveBeenCalledTimes(1)
     expect(routerMock.push).toHaveBeenCalledWith('/tutorials/category/1')
+
+    const showSnackbarSpy = jest.spyOn(wrapper.vm, 'showSnackbar')
+    wrapper.vm.goTo('ThisIsNotValid')
+
+    await flushPromises()
+
+    expect(showSnackbarSpy).toHaveBeenCalledTimes(1)
   })
 
   it('>> maxHeight', async () => {
+    wrapper.vm.$vuetify.breakpoint.mdAndDown = false
+
+    expect(wrapper.vm.maxHeight).toStrictEqual(700)
+    
+    wrapper.vm.$vuetify.breakpoint.mdAndDown = true
+
     expect(wrapper.vm.maxHeight).toStrictEqual(700)
   })
 })
