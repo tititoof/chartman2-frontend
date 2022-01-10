88c862f249bfc06e7adc19edc5e2dde2cdac7baf
Christophe Hartmann <chartmann.35@gmail.com>
2022-01-10 13:01:17 +0100

    fix(ui): first line padding y

--- components/Home/HomeList.vue
+++ components/Home/HomeList.vue
@@ -8,7 +8,7 @@
     >
       <v-container class="fill-height px-4 py-12 justify-center">
         <v-responsive
-          class="d-flex px-4 py-12"
+          class="d-flex px-4 py-6"
           height="100%"
           max-width="1400"
           width="100%"
