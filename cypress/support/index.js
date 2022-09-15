cf46c947c965e8d507808684c3c73838a2cdbf05
Christophe Hartmann <chartmann.35@gmail.com>
2022-09-15 09:09:28 +0200

    fix(lint): errors in some files

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,15 +1,15 @@
-Cypress.on("window:before:load", (win) => {
+Cypress.on('window:before:load', (win) => {
   win.handleFromCypress = function (request) {
     return fetch(request.url, {
       method: request.method,
       headers: request.requestHeaders,
-      body: request.requestBody,
+      body: request.requestBody
     }).then((res) => {
-      let content = res.headers.get("content-type").includes("application/json")
+      const content = res.headers.get('content-type').includes('application/json')
         ? res.json()
         : res.text()
       return new Promise((resolve) => {
-        content.then((body) => resolve([res.status, res.headers, body]))
+        content.then(body => resolve([res.status, res.headers, body]))
       })
     })
   }
